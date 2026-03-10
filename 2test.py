eb0e2fbf67bf2a4318b0d6cbcd0d93b0faa62c64
MingGe0 <ming571232728@gmail.com>
2026-03-10 09:46:28 +0800

    第一次提交

---
+++ 2test.py
@@ -0,0 +1,36 @@
+# 练习2.3
+import this
+name = "Eric"
+print(f"hello {name},would you like to learn some python today?")
+# 练习2.4
+print(name.lower())
+print(name.upper())
+print(name.title())
+# 练习2.5
+print('Albert Einstein once said,"A person who never made a mistake never tried anything new"')
+# 练习2.6
+famous_person = "albert einstein"
+message = f'{famous_person.title()} once said,"A person who never made a mistake never tried anything new"'
+print(message)
+# 练习2.7
+name_2 = "\tma ke si\n"
+print(name_2)
+print('lstrip()')
+print(name_2.lstrip())
+print('rstrip()')
+print(name_2.rstrip())
+print('strip')
+print(name_2.strip())
+# 练习2.8
+filename = 'python_nates.txt'
+simple_filename = filename.removesuffix('.txt')
+print(simple_filename)
+# 练习2.9
+print(2+6)
+print(2*4)
+print(10-2)
+print(16/2)
+# 练习2.10
+love_integer = 7
+mes = f'我最喜欢的数字是:{love_integer}.'
+print(mes)
